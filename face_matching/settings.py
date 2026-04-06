64b473ea0f921564c8d19d32818e44679d5ee44c
Bhagath <bhagathsamalla0@gmail.com>
2026-04-06 22:55:43 +0530

    all origin CORS

--- face_matching/settings.py
+++ face_matching/settings.py
@@ -135,6 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Settings
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = _get_list_env(
     'CORS_ALLOWED_ORIGINS',
     [
